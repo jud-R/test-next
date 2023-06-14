ad97a8328cb5ccb412fcab55160dc1c3bf91544d
Jud <r.judicael@gmail.com>
2023-06-14 17:40:18 +0200

    rendering with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,35 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import {useEffect, useState} from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({posts}) {
+
   return (
     <>
     <Head>
       <title>Test Next - Blog</title>
     </Head>
+    <ul>
+        {posts.map( post => (
+            <li key={post.id}>
+                <h3>{post.title}</h3>
+            </li>
+        ))}
+    </ul>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const posts =  await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  .then(response => response.json())
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
